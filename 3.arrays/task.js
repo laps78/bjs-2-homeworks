7d928374711096361e54108b6b4093bf90d254be
laps78 <9254384@mail.ru>
2022-05-18 13:34:59 +0300

    Tasks of 3.Arrays solved NoNo 1,2. All tests are successful.

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  result = JSON.stringify(arr1) === JSON.stringify(arr2);
 
   return result; // boolean
 }
@@ -10,6 +11,7 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr.filter((item) => item > 0).filter((item) => item % 3 === 0).map((item) => item * 10);
 
   return resultArr; // array
 }
