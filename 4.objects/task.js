a2664d7eb986f65daab3d720c5d1865056bf2adb
laps78 <9254384@mail.ru>
2022-05-23 23:20:56 +0300

    initial commit: training tasks fo object chapter started. Some code wroten but not working. Tests end with mistakes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,26 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark,];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks(...marks) {
+  for (let mark in marks) {
+    this.marks.push(mark);
+  }
+}
