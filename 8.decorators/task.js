9e7a6b87fdb9962d00af1231008e00f8455dc6a3
laps78 <9254384@mail.ru>
2022-06-15 21:18:56 +0300

    8.decorators: first task stuckgit push!

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,33 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+  let cache = [];
+  function wrapper(...args) {
+    const hash = args.join(',');    
+    
+        //
+    console.log(`hash: ${hash}`);
+    console.log(`cache: ${cache}`);
+        //
+    
+    let index = cache.findIndex((item) => item === hash);
+    if (index !== -1) {
+      console.log(`Из кэша: ${cache[index]}`);
+    } else {
+      let result = func(...args);
+      if (cache.length >= 5) {
+        cache.shift();
+      }
+      cache.push(hash);
+      
+          //
+      console.log(`cache: ${cache}`);
+          //
+
+      console.log(`Вычисляем: ${result}`);
+    }
+  }
+  return wrapper;
+
 }
 
 
