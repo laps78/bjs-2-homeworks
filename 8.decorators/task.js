7c2c5b14c6fc6c2830823c3fbca85e2e5fd82709
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-06-16 19:25:38 +0300

    8.decorators: all tasks sended to code review 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,52 +3,64 @@ function cachingDecoratorNew(func) {
   let cache = [];
   function wrapper(...args) {
     const hash = args.join(',');
-    let index = cache.findIndex((item) => item.hash === hash);
+    let index = cache.find((item) => item.hash === hash);
 
-    if (index !== -1) {
-      console.log(`Из кэша: ${cache[index].result}`);
-      return `Из кэша: ${cache[index].result}`;
-    } else {
-      let result = func(...args);
-      if (cache.length >= 5) {
-        cache.shift();
-      };
-      cache.push({hash, result});//???
-      console.log(`Вычисляем: ${result}`);
-      return `Вычисляем: ${result}`;
-    }
+    if (index) {
+      console.log(`Из кэша: ${index.result}`);
+      return `Из кэша: ${index.result}`;
+    };
+    let result = func(...args);
+    if (cache.length >= 5) {
+      cache.shift();
+    };
+    cache.push({ hash, result });
+    console.log(`Вычисляем: ${result}`);
+    return `Вычисляем: ${result}`;
   }
   return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, delay) {
   // Ваш код
-  func(args);
-  let isCalled = true;
+  
   let timeout;
 
-  return function () {
-    if (isCalled) {
-      return;
-    }
-
-    //
-    console.log(`args: ${args}`);
-    console.log(`args[1]: ${args[1]}`);
-    //
-
+  return function (...args) {
+    func(args);
+    let isCalled = true;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
-      func.apply(this, args);
-      console.log(`func apply`);
-    }, args[1]);
+      if (!isCalled) {
+        func.apply(this, args);
+        isCalled = false;
+      }
+    }, delay);
   };
 
 }
 
 function debounceDecorator2(func) {
   // Ваш код
-    
-    //еще не приступал...
+  let timeout;
+
+  return function (...args) {
+    if (!this.count) {
+      this.count = 0;
+    }
+
+    func(args);
+
+    this.count++
+    let isCalled = true;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (!isCalled) {
+        func.apply(this, args);
+        isCalled = false;
+      }
+    }, delay);
+  };
+
 }
