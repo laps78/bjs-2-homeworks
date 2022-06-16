d5a4f97877ac827e61a885278e59cdc8850e56f2
laps78 <9254384@mail.ru>
2022-06-16 11:23:23 +0300

    8.decorators: 1st task is solved, all tests are succesful! Only two tasks left...

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -2,37 +2,29 @@ function cachingDecoratorNew(func) {
   // Ваш код
   let cache = [];
   function wrapper(...args) {
-    const hash = args.join(',');    
-    
-        //
-    console.log(`hash: ${hash}`);
-    console.log(`cache: ${cache}`);
-        //
-    
-    let index = cache.findIndex((item) => item === hash);
+    const hash = args.join(',');
+    let index = cache.findIndex((item) => item.hash === hash);
+
     if (index !== -1) {
-      console.log(`Из кэша: ${cache[index]}`);
+      console.log(`Из кэша: ${cache[index].result}`);
+      return `Из кэша: ${cache[index].result}`;
     } else {
       let result = func(...args);
       if (cache.length >= 5) {
         cache.shift();
-      }
-      cache.push(hash);
-      
-          //
-      console.log(`cache: ${cache}`);
-          //
-
+      };
+      cache.push({hash, result});//???
       console.log(`Вычисляем: ${result}`);
+      return `Вычисляем: ${result}`;
     }
   }
   return wrapper;
-
 }
 
 
 function debounceDecoratorNew(func) {
   // Ваш код
+  
 }
 
 function debounceDecorator2(func) {
