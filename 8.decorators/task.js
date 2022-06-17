5aa0027e5388e23302e7deacadb4086d18ab39fc
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2022-06-17 20:59:06 +0300

    8.async: next fix iteration: some logics of debounceDecorators was changed.

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,11 +3,11 @@ function cachingDecoratorNew(func) {
   let cache = [];
   function wrapper(...args) {
     const hash = args.join(',');
-    let index = cache.find((item) => item.hash === hash);
+    let elem = cache.find((item) => item.hash === hash);
 
-    if (index) {
-      console.log(`Из кэша: ${index.result}`);
-      return `Из кэша: ${index.result}`;
+    if (elem) {
+      console.log(`Из кэша: ${elem.result}`);
+      return `Из кэша: ${elem.result}`;
     };
     let result = func(...args);
     if (cache.length >= 5) {
@@ -20,15 +20,13 @@ function cachingDecoratorNew(func) {
   return wrapper;
 }
 
-
 function debounceDecoratorNew(func, delay) {
   // Ваш код
-  
+  func(...args);
   let timeout;
+  let isCalled = true;
 
   return function (...args) {
-    func(args);
-    let isCalled = true;
     clearTimeout(timeout);
     timeout = setTimeout(() => {
       if (!isCalled) {
@@ -37,24 +35,23 @@ function debounceDecoratorNew(func, delay) {
       }
     }, delay);
   };
-
 }
 
-function debounceDecorator2(func) {
+function debounceDecorator2(func, delay) {
   // Ваш код
   let timeout;
-
+  func(...args);
+  let isCalled = true;
+  
   return function (...args) {
     if (!this.count) {
       this.count = 0;
     }
-
-    func(args);
-
+    
     this.count++
-    let isCalled = true;
-
+    
     clearTimeout(timeout);
+    
     timeout = setTimeout(() => {
       if (!isCalled) {
         func.apply(this, args);
